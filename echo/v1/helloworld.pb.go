42654ad85f6d68d2e627c43ef77b6b923c94d3ab
mobingideployer <dev@mobingi.com>
2021-01-25 17:57:21 +0000

    refs/heads/main 8a5630ab91fc848ed4a2e60478d017cef8f6dca0

--- echo/v1/helloworld.pb.go
+++ echo/v1/helloworld.pb.go
@@ -27,6 +27,7 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
+// Request
 type HelloRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
