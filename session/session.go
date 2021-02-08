1ae628563b2dc4f7915d7902e8347a000df88d4d
flowerinthenight <flowerinthenight@pm.me>
2021-02-09 00:24:08 +0900

    chore: add endpoints

--- session/session.go
+++ session/session.go
@@ -12,6 +12,9 @@ import (
 const (
 	LoginUrlRipple = "https://login.alphaus.cloud/ripple/access_token"
 	LoginUrlWave   = "https://login.alphaus.cloud/access_token"
+
+	BlueAwsEndpoint = "blueawsd-production-u554nqhjka-an.a.run.app:443"
+	AwsCostEndpoint = "awscostd-production-u554nqhjka-an.a.run.app:443"
 )
 
 type Option interface {
