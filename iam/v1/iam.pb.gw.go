e57bd569b3df1bab3dc532ddfd3461e97f036cbc
mobingideployer <dev@mobingi.com>
2021-06-01 16:56:12 +0000

    refs/heads/main 83dc281d8a1959d66ba81bbb7f44f813564e4537

--- iam/v1/iam.pb.gw.go
+++ iam/v1/iam.pb.gw.go
@@ -143,7 +143,7 @@ func RegisterIamHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 }
 
 var (
-	pattern_Iam_WhoAmI_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "iam", "whoami"}, ""))
+	pattern_Iam_WhoAmI_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "whoami"}, ""))
 )
 
 var (
