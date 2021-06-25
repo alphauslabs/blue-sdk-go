f9e0e1a290817a461405d3133c030c6a1109ca7a
mobingideployer <dev@mobingi.com>
2021-06-25 15:46:51 +0000

    refs/heads/main 0d5cb8089d0591e271279018fe6c049f3643400d

--- cost/v1/cost.pb.gw.go
+++ cost/v1/cost.pb.gw.go
@@ -191,6 +191,14 @@ func request_Cost_ImportCurFiles_0(ctx context.Context, marshaler runtime.Marsha
 	var protoReq ImportCurFilesRequest
 	var metadata runtime.ServerMetadata
 
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
 	msg, err := client.ImportCurFiles(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
@@ -199,6 +207,14 @@ func request_Cost_ImportCurFiles_0(ctx context.Context, marshaler runtime.Marsha
 func local_request_Cost_ImportCurFiles_0(ctx context.Context, marshaler runtime.Marshaler, server CostServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ImportCurFilesRequest
 	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.ImportCurFiles(ctx, &protoReq)
 	return msg, metadata, err
