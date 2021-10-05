49cf4d4d9bcb0ec7dbf007b2ee6f4d665d6eb3cc
mobingideployer <dev@mobingi.com>
2021-10-05 16:17:04 +0000

    refs/heads/main 02146b53e8657f0c8fa4da74717954c3555fadf2

--- cost/v1/cost.pb.gw.go
+++ cost/v1/cost.pb.gw.go
@@ -578,6 +578,10 @@ func local_request_Cost_DeleteAccount_0(ctx context.Context, marshaler runtime.M
 
 }
 
+var (
+	filter_Cost_GetCalculatorStatus_0 = &utilities.DoubleArray{Encoding: map[string]int{"vendor": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
 func request_Cost_GetCalculatorStatus_0(ctx context.Context, marshaler runtime.Marshaler, client CostClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetCalculatorStatusRequest
 	var metadata runtime.ServerMetadata
@@ -597,6 +601,13 @@ func request_Cost_GetCalculatorStatus_0(ctx context.Context, marshaler runtime.M
 	protoReq.Vendor, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "vendor", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Cost_GetCalculatorStatus_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetCalculatorStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -623,6 +634,13 @@ func local_request_Cost_GetCalculatorStatus_0(ctx context.Context, marshaler run
 	protoReq.Vendor, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "vendor", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Cost_GetCalculatorStatus_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetCalculatorStatus(ctx, &protoReq)
