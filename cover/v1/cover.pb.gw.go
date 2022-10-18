07680d2ee76b0f299024bfec5195aa77c664551d
mobingideployer <dev@mobingi.com>
2022-10-18 02:22:35 +0000

    refs/heads/main b6f7384116e42478f4eef2567054a85645a0b4f4

--- cover/v1/cover.pb.gw.go
+++ cover/v1/cover.pb.gw.go
@@ -2516,6 +2516,58 @@ func request_Cover_GetCostUsage_0(ctx context.Context, marshaler runtime.Marshal
 
 }
 
+func request_Cover_GetRightSizingRecommendation_0(ctx context.Context, marshaler runtime.Marshaler, client CoverClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetRightSizingRecommendationRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["costGroupId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "costGroupId")
+	}
+
+	protoReq.CostGroupId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "costGroupId", err)
+	}
+
+	msg, err := client.GetRightSizingRecommendation(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Cover_GetRightSizingRecommendation_0(ctx context.Context, marshaler runtime.Marshaler, server CoverServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetRightSizingRecommendationRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["costGroupId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "costGroupId")
+	}
+
+	protoReq.CostGroupId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "costGroupId", err)
+	}
+
+	msg, err := server.GetRightSizingRecommendation(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterCoverHandlerServer registers the http handlers for service Cover to "mux".
 // UnaryRPC     :call CoverServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -3739,6 +3791,29 @@ func RegisterCoverHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 		return
 	})
 
+	mux.Handle("GET", pattern_Cover_GetRightSizingRecommendation_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/blueapi.cover.v1.Cover/GetRightSizingRecommendation")
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Cover_GetRightSizingRecommendation_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Cover_GetRightSizingRecommendation_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -4880,6 +4955,26 @@ func RegisterCoverHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Cover_GetRightSizingRecommendation_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/blueapi.cover.v1.Cover/GetRightSizingRecommendation")
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Cover_GetRightSizingRecommendation_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Cover_GetRightSizingRecommendation_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -4993,6 +5088,8 @@ var (
 	pattern_Cover_ListAssets_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "assets"}, "read"))
 
 	pattern_Cover_GetCostUsage_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "costusage"}, "read"))
+
+	pattern_Cover_GetRightSizingRecommendation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "rightsizingrecommendation", "costGroupId"}, ""))
 )
 
 var (
@@ -5105,4 +5202,6 @@ var (
 	forward_Cover_ListAssets_0 = runtime.ForwardResponseStream
 
 	forward_Cover_GetCostUsage_0 = runtime.ForwardResponseStream
+
+	forward_Cover_GetRightSizingRecommendation_0 = runtime.ForwardResponseMessage
 )
--- cover/v1/cover_grpc.pb.go
+++ cover/v1/cover_grpc.pb.go
@@ -133,6 +133,8 @@ type CoverClient interface {
 	ListAssets(ctx context.Context, in *ListAssetsRequest, opts ...grpc.CallOption) (Cover_ListAssetsClient, error)
 	// WORK-IN-PROGRESS: Get usage and usage-based costs for the specified costgroup
 	GetCostUsage(ctx context.Context, in *GetCostUsageRequest, opts ...grpc.CallOption) (Cover_GetCostUsageClient, error)
+	// Gets the right sizing recommendation of all the accounts in the costgroup
+	GetRightSizingRecommendation(ctx context.Context, in *GetRightSizingRecommendationRequest, opts ...grpc.CallOption) (*GetRightSizingRecommendationResponse, error)
 }
 
 type coverClient struct {
@@ -707,6 +709,15 @@ func (x *coverGetCostUsageClient) Recv() (*CostItem, error) {
 	return m, nil
 }
 
+func (c *coverClient) GetRightSizingRecommendation(ctx context.Context, in *GetRightSizingRecommendationRequest, opts ...grpc.CallOption) (*GetRightSizingRecommendationResponse, error) {
+	out := new(GetRightSizingRecommendationResponse)
+	err := c.cc.Invoke(ctx, "/blueapi.cover.v1.Cover/GetRightSizingRecommendation", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CoverServer is the server API for Cover service.
 // All implementations must embed UnimplementedCoverServer
 // for forward compatibility
@@ -820,6 +831,8 @@ type CoverServer interface {
 	ListAssets(*ListAssetsRequest, Cover_ListAssetsServer) error
 	// WORK-IN-PROGRESS: Get usage and usage-based costs for the specified costgroup
 	GetCostUsage(*GetCostUsageRequest, Cover_GetCostUsageServer) error
+	// Gets the right sizing recommendation of all the accounts in the costgroup
+	GetRightSizingRecommendation(context.Context, *GetRightSizingRecommendationRequest) (*GetRightSizingRecommendationResponse, error)
 	mustEmbedUnimplementedCoverServer()
 }
 
@@ -991,6 +1004,9 @@ func (UnimplementedCoverServer) ListAssets(*ListAssetsRequest, Cover_ListAssetsS
 }
 func (UnimplementedCoverServer) GetCostUsage(*GetCostUsageRequest, Cover_GetCostUsageServer) error {
 	return status.Errorf(codes.Unimplemented, "method GetCostUsage not implemented")
+}
+func (UnimplementedCoverServer) GetRightSizingRecommendation(context.Context, *GetRightSizingRecommendationRequest) (*GetRightSizingRecommendationResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetRightSizingRecommendation not implemented")
 }
 func (UnimplementedCoverServer) mustEmbedUnimplementedCoverServer() {}
 
@@ -2004,6 +2020,24 @@ func (x *coverGetCostUsageServer) Send(m *CostItem) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _Cover_GetRightSizingRecommendation_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetRightSizingRecommendationRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CoverServer).GetRightSizingRecommendation(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/blueapi.cover.v1.Cover/GetRightSizingRecommendation",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CoverServer).GetRightSizingRecommendation(ctx, req.(*GetRightSizingRecommendationRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Cover_ServiceDesc is the grpc.ServiceDesc for Cover service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -2218,6 +2252,10 @@ var Cover_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RegisterAccount",
 			Handler:    _Cover_RegisterAccount_Handler,
+		},
+		{
+			MethodName: "GetRightSizingRecommendation",
+			Handler:    _Cover_GetRightSizingRecommendation_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
