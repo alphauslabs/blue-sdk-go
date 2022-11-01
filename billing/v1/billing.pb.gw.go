745f54ceda6b7a2b1e367a20221cec43a081a89e
mobingideployer <dev@mobingi.com>
2022-11-01 02:11:45 +0000

    refs/heads/main 5274a9302b0e062d5da03bdc858be20f18a147d8

--- billing/v1/billing.pb.gw.go
+++ billing/v1/billing.pb.gw.go
@@ -31,9 +31,20 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+var (
+	filter_Billing_ListBillingGroups_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Billing_ListBillingGroups_0(ctx context.Context, marshaler runtime.Marshaler, client BillingClient, req *http.Request, pathParams map[string]string) (Billing_ListBillingGroupsClient, runtime.ServerMetadata, error) {
 	var protoReq ListBillingGroupsRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Billing_ListBillingGroups_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	stream, err := client.ListBillingGroups(ctx, &protoReq)
 	if err != nil {
