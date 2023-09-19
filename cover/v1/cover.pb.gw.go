9e224b80b28666f62364b620c4fab1d659883db0
mobingideployer <dev@mobingi.com>
2023-09-19 13:59:22 +0000

    refs/heads/main b63605881aabc4bab8bee6ef9daa48cb49fcbb32

--- cover/v1/cover.pb.gw.go
+++ cover/v1/cover.pb.gw.go
@@ -8493,7 +8493,7 @@ var (
 
 	pattern_Cover_AddMpnSetting_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "azure", "credentials", "mpn"}, ""))
 
-	pattern_Cover_GetCostGroupAttribute_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "costgroups", "attributeType", "costGroupId"}, ""))
+	pattern_Cover_GetCostGroupAttribute_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "attributes", "attributeType", "costGroupId"}, ""))
 )
 
 var (
