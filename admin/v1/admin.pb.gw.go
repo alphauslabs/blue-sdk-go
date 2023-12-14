9111c36c3f151232e369b9f4ecd5dcac86d72d34
mobingideployer <dev@mobingi.com>
2023-12-14 16:11:28 +0000

    refs/heads/main b6fab4aa8b63f5f87493eba4e306d242d7bc178c

--- admin/v1/admin.pb.gw.go
+++ admin/v1/admin.pb.gw.go
@@ -499,6 +499,10 @@ func local_request_Admin_ListNotificationChannels_0(ctx context.Context, marshal
 
 }
 
+var (
+	filter_Admin_GetNotificationChannel_0 = &utilities.DoubleArray{Encoding: map[string]int{"id": 0}, Base: []int{1, 2, 0, 0}, Check: []int{0, 1, 2, 2}}
+)
+
 func request_Admin_GetNotificationChannel_0(ctx context.Context, marshaler runtime.Marshaler, client AdminClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetNotificationChannelRequest
 	var metadata runtime.ServerMetadata
@@ -518,6 +522,13 @@ func request_Admin_GetNotificationChannel_0(ctx context.Context, marshaler runti
 	protoReq.Id, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Admin_GetNotificationChannel_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetNotificationChannel(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -544,6 +555,13 @@ func local_request_Admin_GetNotificationChannel_0(ctx context.Context, marshaler
 	protoReq.Id, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Admin_GetNotificationChannel_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetNotificationChannel(ctx, &protoReq)
