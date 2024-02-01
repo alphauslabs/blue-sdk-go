e63f879e7efac2ba3d869f88a9f2b29b990386a3
mobingideployer <dev@mobingi.com>
2024-02-01 12:54:27 +0000

    refs/heads/main feea7c704e198e127daea391ed0e2aeed0a33f2d

--- cost/v1/cost.pb.gw.go
+++ cost/v1/cost.pb.gw.go
@@ -31,6 +31,10 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+var (
+	filter_Cost_ListPayerAccounts_0 = &utilities.DoubleArray{Encoding: map[string]int{"vendor": 0}, Base: []int{1, 2, 0, 0}, Check: []int{0, 1, 2, 2}}
+)
+
 func request_Cost_ListPayerAccounts_0(ctx context.Context, marshaler runtime.Marshaler, client CostClient, req *http.Request, pathParams map[string]string) (Cost_ListPayerAccountsClient, runtime.ServerMetadata, error) {
 	var protoReq ListPayerAccountsRequest
 	var metadata runtime.ServerMetadata
@@ -50,6 +54,13 @@ func request_Cost_ListPayerAccounts_0(ctx context.Context, marshaler runtime.Mar
 	protoReq.Vendor, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "vendor", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Cost_ListPayerAccounts_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	stream, err := client.ListPayerAccounts(ctx, &protoReq)
