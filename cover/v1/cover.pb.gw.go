57b5d4e8b6a9d986eb51927c3b4fd79d74c8e572
mobingideployer <dev@mobingi.com>
2024-04-18 05:15:40 +0000

    refs/heads/main 5dd1b45b46c4edf496152faeeef910ec31f48e1e

--- cover/v1/cover.pb.gw.go
+++ cover/v1/cover.pb.gw.go
@@ -6116,7 +6116,7 @@ func request_Cover_MarkAsExecuted_0(ctx context.Context, marshaler runtime.Marsh
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
 	}
 
-	protoReq.Id, err = runtime.String(val)
+	protoReq.Id, err = runtime.StringSlice(val, ",")
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
 	}
@@ -6150,7 +6150,7 @@ func local_request_Cover_MarkAsExecuted_0(ctx context.Context, marshaler runtime
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
 	}
 
-	protoReq.Id, err = runtime.String(val)
+	protoReq.Id, err = runtime.StringSlice(val, ",")
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
 	}
