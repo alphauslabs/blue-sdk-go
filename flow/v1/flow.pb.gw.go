fc219ee2b294c09f1b86edd771cfc5df3a8c6b25
mobingideployer <dev@mobingi.com>
2025-09-10 06:49:12 +0000

    refs/heads/main a7208f3b3e86ef97ebba080699bd800a9982c40c

--- flow/v1/flow.pb.gw.go
+++ flow/v1/flow.pb.gw.go
@@ -452,6 +452,8 @@ func local_request_Flow_CreateMessageToSlack_0(ctx context.Context, marshaler ru
 	return msg, metadata, err
 }
 
+var filter_Flow_GetSPToPurchaseDetails_0 = &utilities.DoubleArray{Encoding: map[string]int{"payerId": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+
 func request_Flow_GetSPToPurchaseDetails_0(ctx context.Context, marshaler runtime.Marshaler, client FlowClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq GetSPToPurchaseDetailsRequest
@@ -468,6 +470,12 @@ func request_Flow_GetSPToPurchaseDetails_0(ctx context.Context, marshaler runtim
 	protoReq.PayerId, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "payerId", err)
+	}
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Flow_GetSPToPurchaseDetails_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := client.GetSPToPurchaseDetails(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -486,6 +494,12 @@ func local_request_Flow_GetSPToPurchaseDetails_0(ctx context.Context, marshaler 
 	protoReq.PayerId, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "payerId", err)
+	}
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Flow_GetSPToPurchaseDetails_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := server.GetSPToPurchaseDetails(ctx, &protoReq)
 	return msg, metadata, err
