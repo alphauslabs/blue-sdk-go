bf86391e2acecbbf1f72187558f8833cfa0c8604
mobingideployer <dev@mobingi.com>
2025-09-18 02:15:26 +0000

    refs/heads/main dcfaa5426214988d30aa5ce1c0de38370a3957fe

--- flow/v1/flow.pb.gw.go
+++ flow/v1/flow.pb.gw.go
@@ -507,21 +507,16 @@ func local_request_Flow_GetSPToPurchaseDetails_0(ctx context.Context, marshaler 
 	return msg, metadata, err
 }
 
-var filter_Flow_SendSPToPurchaseDetailsNotification_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-
 func request_Flow_SendSPToPurchaseDetailsNotification_0(ctx context.Context, marshaler runtime.Marshaler, client FlowClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq SendSPToPurchaseDetailsNotificationsRequest
 		metadata runtime.ServerMetadata
 	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 	if req.Body != nil {
 		_, _ = io.Copy(io.Discard, req.Body)
-	}
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Flow_SendSPToPurchaseDetailsNotification_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := client.SendSPToPurchaseDetailsNotification(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -532,10 +527,7 @@ func local_request_Flow_SendSPToPurchaseDetailsNotification_0(ctx context.Contex
 		protoReq SendSPToPurchaseDetailsNotificationsRequest
 		metadata runtime.ServerMetadata
 	)
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Flow_SendSPToPurchaseDetailsNotification_0); err != nil {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := server.SendSPToPurchaseDetailsNotification(ctx, &protoReq)
