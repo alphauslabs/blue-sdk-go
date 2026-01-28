663442ee3e9f415c8eeaefec60c6f45075f79c38
mobingideployer <dev@mobingi.com>
2026-01-28 06:10:02 +0000

    refs/heads/main 74b47800e8742c953cc66f01ce7af1dbeda66506

--- billing/v1/billing.pb.gw.go
+++ billing/v1/billing.pb.gw.go
@@ -4813,6 +4813,51 @@ func local_request_Billing_DeleteChildBillingGroup_0(ctx context.Context, marsha
 	return msg, metadata, err
 }
 
+func request_Billing_UpdateChildBillingGroupInvoiceSettings_0(ctx context.Context, marshaler runtime.Marshaler, client BillingClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq UpdateChildBillingGroupInvoiceSettingsRequest
+		metadata runtime.ServerMetadata
+		err      error
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
+	val, ok := pathParams["internalId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "internalId")
+	}
+	protoReq.InternalId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "internalId", err)
+	}
+	msg, err := client.UpdateChildBillingGroupInvoiceSettings(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+}
+
+func local_request_Billing_UpdateChildBillingGroupInvoiceSettings_0(ctx context.Context, marshaler runtime.Marshaler, server BillingServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq UpdateChildBillingGroupInvoiceSettingsRequest
+		metadata runtime.ServerMetadata
+		err      error
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	val, ok := pathParams["internalId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "internalId")
+	}
+	protoReq.InternalId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "internalId", err)
+	}
+	msg, err := server.UpdateChildBillingGroupInvoiceSettings(ctx, &protoReq)
+	return msg, metadata, err
+}
+
 func request_Billing_BulkCreateBillingGroup_0(ctx context.Context, marshaler runtime.Marshaler, client BillingClient, req *http.Request, pathParams map[string]string) (Billing_BulkCreateBillingGroupClient, runtime.ServerMetadata, error) {
 	var (
 		protoReq BulkCreateBillingGroupRequest
@@ -6903,6 +6948,26 @@ func RegisterBillingHandlerServer(ctx context.Context, mux *runtime.ServeMux, se
 			return
 		}
 		forward_Billing_DeleteChildBillingGroup_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
+	mux.Handle(http.MethodPut, pattern_Billing_UpdateChildBillingGroupInvoiceSettings_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/blueapi.billing.v1.Billing/UpdateChildBillingGroupInvoiceSettings", runtime.WithHTTPPathPattern("/v1/billinggroups/child/{internalId}/invoiceSettings"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Billing_UpdateChildBillingGroupInvoiceSettings_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_Billing_UpdateChildBillingGroupInvoiceSettings_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	mux.Handle(http.MethodPost, pattern_Billing_BulkCreateBillingGroup_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -9025,6 +9090,23 @@ func RegisterBillingHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 		}
 		forward_Billing_DeleteChildBillingGroup_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodPut, pattern_Billing_UpdateChildBillingGroupInvoiceSettings_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/blueapi.billing.v1.Billing/UpdateChildBillingGroupInvoiceSettings", runtime.WithHTTPPathPattern("/v1/billinggroups/child/{internalId}/invoiceSettings"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Billing_UpdateChildBillingGroupInvoiceSettings_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_Billing_UpdateChildBillingGroupInvoiceSettings_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	mux.Handle(http.MethodPost, pattern_Billing_BulkCreateBillingGroup_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -9168,6 +9250,7 @@ var (
 	pattern_Billing_ListChildBillingGroups_0                     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "billinggroups", "child"}, "read"))
 	pattern_Billing_UpdateChildBillingGroup_0                    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "billinggroups", "child", "internalId"}, ""))
 	pattern_Billing_DeleteChildBillingGroup_0                    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "billinggroups", "child", "internalId"}, ""))
+	pattern_Billing_UpdateChildBillingGroupInvoiceSettings_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"v1", "billinggroups", "child", "internalId", "invoiceSettings"}, ""))
 	pattern_Billing_BulkCreateBillingGroup_0                     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "billinggroups"}, "bulkCreate"))
 )
 
@@ -9294,5 +9377,6 @@ var (
 	forward_Billing_ListChildBillingGroups_0                     = runtime.ForwardResponseStream
 	forward_Billing_UpdateChildBillingGroup_0                    = runtime.ForwardResponseMessage
 	forward_Billing_DeleteChildBillingGroup_0                    = runtime.ForwardResponseMessage
+	forward_Billing_UpdateChildBillingGroupInvoiceSettings_0     = runtime.ForwardResponseMessage
 	forward_Billing_BulkCreateBillingGroup_0                     = runtime.ForwardResponseStream
 )
