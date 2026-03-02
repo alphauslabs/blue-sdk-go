7bfd7751e74eabec21f631b082dec0dcd4fbb88c
mobingideployer <dev@mobingi.com>
2026-03-02 13:37:51 +0000

    refs/heads/main 1e95cb303f628ea1a303ba21cbcd018400addf42

--- vortex/v1/vortexconnect/vortex.connect.go
+++ vortex/v1/vortexconnect/vortex.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	vortex "github.com/alphauslabs/blueapi/vortex"
+	vortex "github.com/alphauslabs/blue-sdk-go/vortex"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	http "net/http"
 	strings "strings"
